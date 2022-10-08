3facf8c6322044b63abb571b699e7c4b047cf1e5
Isntitfun <112168128+Isntitfun@users.noreply.github.com>
2022-10-08 20:28:49 +0900

    Created_(Gallery)

--- practice project/nav-bar/javascript/app.js
+++ practice project/nav-bar/javascript/app.js
@@ -2,10 +2,23 @@ const navToggle = document.querySelector(".nav-toggle");
 const navContents = document.querySelector(".nav-contents");
 const contacts = document.querySelector(".contacts");
 const nav = document.querySelector("nav");
+const links = document.querySelectorAll(".nav-contents a");
 
 function handleNavToggleClick() {
   navContents.classList.toggle("nav-show");
   contacts.classList.toggle("toggle-active");
   nav.classList.toggle("toggle-active");
 }
+
+function handleLinkClick() {
+  navContents.classList.remove("nav-show");
+  contacts.classList.remove("toggle-active");
+  nav.classList.remove("toggle-active");
+}
+
+function forEachLinks(item) {
+  item.addEventListener("click", handleLinkClick);
+}
+
 navToggle.addEventListener("click", handleNavToggleClick);
+links.forEach((item) => forEachLinks(item));
